c611de12ce906570dacc4b7e1f5146d642605811
cha53c <chasec@9JZK85J.vcint.local>
2015-02-25 16:48:41 +0100

    added chef to install

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,15 @@ apt-get install -y git
 apt-get install -y curl
 apt-get install -y vim
 
+
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
+
+
+curl -L https://www.opscode.com/chef/install.sh | bash
 
 rvm reload
 rvm install 2.2.0
 
 gem install bundler rails
+gem install chef --no-ri --no-rdoc
